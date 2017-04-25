a94075f6d2f0289128f3c51634bdcde88dd678d8
Kirill Bond <code.cyrbon@gmail.com>
2017-04-25 16:51:42 +0300

    Add `add` to SuiteM

--- src/Benchmark/Suite/Monad.purs
+++ src/Benchmark/Suite/Monad.purs
@@ -5,18 +5,21 @@ module Benchmark.Suite.Monad
   , SuiteT
   -- General
   , runSuiteT
+  -- API Wrappers
+  , add
   ) where
 
 import Prelude
 import Control.Monad.ST as ST
-import Control.Monad.Reader (runReaderT, ReaderT, class MonadReader)
+import Control.Monad.Reader (runReaderT, ReaderT, class MonadReader, ask)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Class (class MonadEff)
+import Control.Monad.Eff.Class (class MonadEff, liftEff)
 import Benchmark.Suite.ST as STS
 import Benchmark.Suite.ST (STSuite)
 import Benchmark.Suite (Suite, pureST)
 import Data.Newtype (class Newtype)
 import Unsafe.Coerce (unsafeCoerce)
+import Control.Applicative ((*>))
 
 -- Types
 --------------------
@@ -48,3 +51,33 @@ runSuiteT (SuiteT m) = pureST do
   let eff = (runReaderT m) (unsafeCoerce s)
   _ <- unsafeCoerce $ eff
   pure s
+
+-- Internal helpers
+--------------------
+
+-- | Converts an `f` with two argumens (first accepting `STSuite s`) into an `f`
+-- | that uses SuiteM
+asksSTSuiteA2 :: forall s m e a2 b.
+     (STSuite s -> a2 -> Eff ( st :: ST.ST s | e ) b)
+  -> a2 -> (SuiteM s m e (m b))
+asksSTSuiteA2 fA2 a2 = do
+  s <- ask
+  liftEff $ fA2 s a2
+
+-- | Converts an `f` with three arguments (first accepting `STSuite s`) into an i
+-- | `f` that uses SuiteM
+asksSTSuiteA3 :: forall s m e a2 a3 b.
+     (STSuite s -> a2 -> a3 -> Eff ( st :: ST.ST s | e ) b)
+  -> (SuiteM s m e (a2 -> a3 -> m b))
+asksSTSuiteA3 fA3 a2 a3 = do
+  s <- ask
+  liftEff $ fA3 s a2 a3
+
+-- Suite API wrappers
+--------------------
+
+-- | Adds a test to the benchmark suite. Takes a name to identify the benchmark,
+-- | and the test to benchmark.
+add :: forall s m e. SuiteM s m e (String -> Eff e Unit -> m Unit)
+add = asksSTSuiteA3 STS.add
+
