c5d9f814b465aa762aa2461dea08368f56ba7965
Kirill Bond <code.cyrbon@gmail.com>
2017-05-14 21:52:30 +0300

    Add runEff, fn to BenchmarkAPI

--- src/Benchmark.purs
+++ src/Benchmark.purs
@@ -1,19 +1,30 @@
 module Benchmark
   ( module Benchmark.Suite.Monad
   , module Benchmark.Output
+  , module Benchmark.Function
   , module Suite
   , module STModule
+  , runBench
+  , fnEff
+  , fn
   ) where
 
 import Benchmark.Suite.Monad
 import Benchmark.Suite (Suite) as Suite
 import Benchmark.Suite.ST (BenchmarkResult) as STModule
 import Benchmark.Output
+import Benchmark.Function (fn1)
 
-import Prelude hiding (add)
+import Prelude (Unit, ($), (*>))
 import Control.Monad.ST as ST
 import Control.Monad.Eff (Eff)
 
-runBench :: forall s m e a.
+runBench :: forall s e a.
   SuiteT s (st :: ST.ST s | e ) a -> Eff (st :: ST.ST s | e ) Unit
 runBench m = runSuiteM $ m *> printResultTableOnComplete
+
+fnEff :: forall s m e anyEff a. SuiteM s e m (String -> Eff anyEff a -> m Unit)
+fnEff = add
+
+fn :: forall s m e a b. SuiteM s e m (String -> (a -> b) -> a -> m Unit)
+fn s f a = add s (fn1 f a)
